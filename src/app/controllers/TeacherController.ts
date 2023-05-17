30509a6d0018f44bf389ecf633eb45076ef0dfaf
FelipeR96 <felipereinaleso96@gmail.com>
2023-05-16 20:48:28 -0500

    Creacion del controlador y las rutas para el profesor

--- src/app/controllers/TeacherController.ts
+++ src/app/controllers/TeacherController.ts
@@ -1,1 +1,33 @@
- 
+import { Teacher } from './../../domain/entities/Teacher';
+import { ITeacherRepository } from "domain/interfaces/ITeacherRepository";
+import { Request, Response } from "express";
+import { json } from 'stream/consumers';
+
+export class TeacherController {
+
+    constructor(private _teacherRepository: ITeacherRepository) {
+    }
+
+    public async create(request: Request, response: Response): Promise<Response> {
+
+        try {
+
+            const { name, description, email, birthDate } = request.body;
+            const teacher: Teacher = new Teacher(name, description, email, new Date(birthDate));
+            const createdTeacher = await this._teacherRepository.create(teacher);
+            return response.status(201).json(createdTeacher);
+
+        } catch (error) {
+
+            console.log(error);// nunca mostrar los errores, ahora por que estamos explicando
+            return response.status(500).json(
+                {
+                    errorMessage: 'Houston tenemos un problema',
+                    code: 9999
+                }
+            );
+
+        }
+
+    }
+}
--- src/app/routes/TeacherRoutes.ts
+++ src/app/routes/TeacherRoutes.ts
@@ -1,1 +1,15 @@
- 
+import { TeacherController } from './../controllers/TeacherController';
+import { TeacherRepository } from './../../infra/repositories/TeacherRepository';
+import { Router } from "express";
+
+ export class TeacherRoutes {
+    private _router: Router;
+    private _teacherRepository: TeacherRepository;
+    private _teacherController: TeacherController;
+
+    constructor() {
+        this._router = Router();
+        this._teacherRepository = new TeacherRepository();
+        this._teacherController = new TeacherController(this._teacherRepository)
+    }
+ }
